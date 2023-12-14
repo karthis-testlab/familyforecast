1041de9d210db9bced624516a1a9d0f6d5362b51
Karthikeyan Rajendran <karthis.testlab@gmail.com>
2023-12-15 01:03:44 +0530

    Adding Spring Security in application with Basic auth

--- src/main/java/com/app/FamilyForeCast/FamilyForeCastSecurityConfig.java
+++ src/main/java/com/app/FamilyForeCast/FamilyForeCastSecurityConfig.java
@@ -1,4 +1,44 @@
 package com.app.FamilyForeCast;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
 public class FamilyForeCastSecurityConfig {
+
+    @Bean
+    SecurityFilterChain familyForeCastBasicAuthenticationAndAuthorization(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(
+                request -> request.requestMatchers("/cashcards/**")
+                        .authenticated()
+        ).httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    UserDetailsService testOnlyUsers(PasswordEncoder passwordEncoder) {
+        User.UserBuilder testUsers = User.builder();
+        UserDetails admin = testUsers
+                .username("admin")
+                .password(passwordEncoder.encode("test@1234"))
+                .roles()
+                .build();
+
+        return new InMemoryUserDetailsManager(admin);
+    }
+
 }
